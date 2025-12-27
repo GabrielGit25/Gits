42253f32e6d6b59f1ab886c271f5ac459a72ba4b
GabrielGit25 <delimacruzgabriel@gmail.com>
2025-12-27 15:13:42 -0300

    Delete shanzam.sh

--- shanzam.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo 'alias shanzam="curl -s https://raw.githubusercontent.com/GabrielGit25/Gits/refs/heads/main/gits.txt"' >> ~/.bash_profile
-source ~/.bash_profile
-echo "🎉 shanzam instalado! 
-
