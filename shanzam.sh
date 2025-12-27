a00140ec57cac22bfa6319c76d50c1cd2e8e25e0
GabrielGit25 <delimacruzgabriel@gmail.com>
2025-12-27 15:05:19 -0300

    shanzam.sh

---
+++ shanzam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo 'alias shanzam="curl -s https://raw.githubusercontent.com/GabrielGit25/Gits/refs/heads/main/gits.txt"' >> ~/.bash_profile
+source ~/.bash_profile
+echo "🎉 shanzam instalado! 
+
